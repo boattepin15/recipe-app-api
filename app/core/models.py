8eccce727c178952b80caec095a0e6dcab76cd9e
boattepin15 <boatbot.seven@gmail.com>
2024-04-20 14:04:36 +0700

    create manager user model and add model user

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for users"""
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return a new superuser."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        # Ensure the superuser has the necessary permissions set
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        # Create a new user with specified superuser fields
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
